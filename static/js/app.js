a0e0cb8748114124509754d94d9ae4f0f7f71793
Armaghan Orakzai <armaghan.orakzai15@gmail.com>
2021-03-20 14:30:40 +0300

    added projects-section

--- static/js/app.js
+++ static/js/app.js
@@ -5,3 +5,20 @@ const links = document.querySelectorAll(".nav-links li");
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("open");
 });
+
+// Smooth scrolling with jQuery
+// Smooth Scrolling
+$("#navbar a, .btn").on("click", function (e) {
+  if (this.hash !== "") {
+    e.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - 70,
+      },
+      800
+    );
+  }
+});
